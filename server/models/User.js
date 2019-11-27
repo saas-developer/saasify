3db69e6e765a451b23cfd8a32190ded036af31d6
Admin <ashwin.puthige@paypal.com>
2019-11-27 11:45:10 -0800

    5.1 Registration - Password Encryption

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 
 let User;
@@ -11,6 +12,33 @@ if (!User) {
 	},
 	{
 		timestamps: true
+	});
+
+
+	userSchema.pre('save', function(next) {
+		// this
+		const user = this;
+
+		const SALT_FACTOR = 5;
+
+		if (!user.isModified('password')) {
+			return next();
+		}
+
+		bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+			if (err) {
+				return next(err);
+			}
+
+			bcrypt.hash(user.password, salt, function(err, hash) {
+				if (err) {
+					return next(err);
+				}
+				user.password = hash;
+
+				next();
+			})
+		})
 	});
 
 	User = mongoose.model('User', userSchema);	
