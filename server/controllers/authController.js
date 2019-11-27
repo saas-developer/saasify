6c7050e922bc734eec101e0805c4f6da57b10bdd
Admin <ashwin.puthige@paypal.com>
2019-11-27 15:38:55 -0800

    6. Login API

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,6 @@
 const User = require('../models/User');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res, next) => {
 	console.log('req.body ', req.body);
@@ -85,6 +87,79 @@ exports.register = async (req, res, next) => {
 	}
 	
 
+
+}
+
+exports.login = async (req, res, next) => {
+	const {
+		email,
+		password
+	} = req.body;
+	// Validate the input fields
+	const validationErrors = [];
+
+	if (!email) {
+		validationErrors.push({
+			code: 'VALIDATION_ERROR',
+			field: 'email',
+			message: ' You must provide an email address'
+		});
+	}
+
+	const isEmailValid = email && validateEmail(email);
+	if (email && !isEmailValid) {
+		validationErrors.push({
+			code: 'VALIDATION_ERROR',
+			field: 'email',
+			message: 'Email is not valid'
+		});
+	}
+
+
+	if (!password) {
+		validationErrors.push({
+			code: 'VALIDATION_ERROR',
+			field: 'password',
+			message: ' You must provide a password'
+		});
+	}
+
+	if (validationErrors.length) {
+		const errorObject = {
+			error: true,
+			errors: validationErrors
+		};
+
+		res.status(422).send(errorObject);
+
+		return;
+	}
+
+	passport.authenticate('local', (err, user, info) => {
+		if (err) {
+			return next(err);
+		}
+
+		if (!user) {
+			res.status(401).send(info);
+			return;			
+		}
+		
+		const userObject = user.toObject();
+
+		const jwtToken = jwt.sign(userObject, process.env.JWT_SECRET || 'TEMP_JWT_SECRET', {
+			expiresIn: 86400 // seconds in a day
+		})
+
+		res.status(200).send({
+			token: jwtToken,
+			user: userObject
+		});
+		return;
+		
+
+
+	})(req, res, next);
 
 }
 
