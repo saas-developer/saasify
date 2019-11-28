18d22aa0197890935888726b275182a44bf6e1a5
Admin <ashwin.puthige@paypal.com>
2019-11-27 16:14:05 -0800

    8. JWT Auth check

--- server/lib/passport.js
+++ server/lib/passport.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/User');
 
 const localOptions = {
@@ -47,6 +49,51 @@ const localLogin = new LocalStrategy(localOptions, function(email, password, don
 
 	});
 });
+
+
+const jwtOpts = {}
+// jwtOpts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+
+jwtOpts.jwtFromRequest = (req) => {
+	const cookies = req.cookies;
+	const token = cookies.token;
+
+	if (token) {
+		return token;
+	}
+
+	const headers = req.headers || {};
+	const authHeader = headers.authorization || '';
+	const headerToken = authHeader.split(' ')[1];
+	if (headerToken) {
+		return headerToken;
+	}
+
+
+	return null;
+}
+
+jwtOpts.secretOrKey = process.env.JWT_SECRET || 'TEMP_JWT_SECRET';
+
+passport.use(new JwtStrategy(jwtOpts, function(jwtPayload, done) {
+	console.log('jwtPayload ', jwtPayload);
+
+	const userId = jwtPayload._id;
+
+    User.findOne({ _id: userId }, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false, {
+            	code: 'GLOBAL_ERROR',
+            	message: 'Email not associated with any account'
+            });
+        }
+    });
+}));
 
 
 passport.use(localLogin);
