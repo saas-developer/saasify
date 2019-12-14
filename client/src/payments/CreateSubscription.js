33000b76666f58696d93925796a1ca2b8639cc98
Admin <admin@email.com>
2019-12-13 20:28:04 -0800

    CLient - Stripe Create Subscription

--- client/src/payments/CreateSubscription.js
+++ client/src/payments/CreateSubscription.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+const stripe = window.Stripe('pk_test_cPAYuvk1DKJ0cqm0cm0Zblms');
+
 export default class CreateSubscription extends React.Component {
   state = {
     selectedPlan: ''
@@ -15,7 +17,43 @@ export default class CreateSubscription extends React.Component {
   }
 
   handleSubmitClick = () => {
+    // Create Checkout Session
+    const url = '/api/payments/session';
+    
+    fetch(url, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      credentials: 'same-origin',
+      method: 'POST',
+      body: JSON.stringify({
+        planId: this.state.selectedPlan
+      })
+    }).then((response) => {
+      if (!response.ok) {
+        return response.json().then(err => { throw err })
+      }
+      return response.json();
+    }).then((results) => {
+      console.log('results ', results);
+      const checkoutSessionId = results.session.id;
 
+      stripe.redirectToCheckout({
+        // Make the id field from the Checkout Session creation API response
+        // available to this file, so you can provide it as parameter here
+        // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+        sessionId: checkoutSessionId
+      }).then(function (result) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer
+        // using `result.error.message`.
+        console.log('result.error.message', result.error.message);
+      });
+      
+    }).catch((error) => {
+      console.log('error ', error);
+    })
   }
 
   render() {
@@ -29,6 +67,7 @@ export default class CreateSubscription extends React.Component {
               value={this.state.selectedPlan}
               onChange={this.handlePlanChange}
             >
+              <option value="">Select a Plan</option>
               <option value="stripe-plan-basic">Basic</option>
               <option value="stripe-plan-enterprise">Enterprise</option>
               <option value="stripe-plan-startup">Startup</option>
@@ -37,6 +76,7 @@ export default class CreateSubscription extends React.Component {
           <Button
             variant="info"
             onClick={this.handleSubmitClick}
+            disabled={this.state.selectedPlan === ''}
           >Subscribe</Button>
         </Form>
       </div>
