d71787492e252748dbb899587cf0a61f2d427739
Admin <admin@email.com>
2020-02-07 16:55:13 -0800

    Stripe: Update Card - Implemented Client side

--- client/src/payments/Payments.js
+++ client/src/payments/Payments.js
@@ -3,6 +3,7 @@ import CurrentSubscription from './CurrentSubscription';
 import CurrentCard from './CurrentCard';
 import StripeProviderComponent from './StripeProviderComponent';
 import CreateSubscription from './CreateSubscription';
+import UpdateCard from './UpdateCard';
 
 export default class Payments extends React.Component {
 	state = {
@@ -60,6 +61,34 @@ export default class Payments extends React.Component {
             this.getPaymentDetails();
 		})
 	}
+
+    updateCard = (paymentMethod) => {
+        const url = '/api/payments/cards';
+        
+        fetch(url, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            credentials: 'same-origin',
+            method: 'POST',
+            body: JSON.stringify({
+                paymentMethod
+            })
+        }).then((response) => {
+            if (!response.ok) {
+                return response.json().then(err => { throw err })
+            }
+            return response.json();
+        }).then((results) => {
+            console.log('results ', results);
+            this.getPaymentDetails();
+            
+        }).catch((error) => {
+            console.log('error ', error);
+            this.getPaymentDetails();
+        })
+    }
 
     deleteSubscription = () => {
         const url = '/api/payments/subscriptions';
@@ -149,13 +178,19 @@ export default class Payments extends React.Component {
                 deleteSubscription={this.deleteSubscription}
               />
 			  <CurrentCard card={this.state.card}/>
+              <UpdateCard
+                user={this.props.user}
+                card={this.state.card}
+                onPaymentMethodCreated={this.updateCard}
+              />
 
 			  <CreateSubscription
                   user={this.props.user}
                   card={this.state.card}
                   plan={this.state.plan}
                   setPlan={this.setPlan}
-                  createSubscription={this.createSubscription}
+                  onPaymentMethodCreated={this.createSubscription}
+                  actionButtonText={"Subscribe"}
               />
 			</div>
 		  </div>
---
+++ client/src/payments/UpdateCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import StripeProviderComponent from './StripeProviderComponent';
+
+export default class UpdateCard extends React.Component {
+    render() {
+        return (
+            <Card>
+              <Card.Body>
+                <h4>Update Card</h4>
+                <div>
+                    <StripeProviderComponent
+                        user={this.props.user}
+                        onPaymentMethodCreated={this.props.onPaymentMethodCreated}
+                        actionButtonText={"Update Card"}
+                    />
+                </div>
+              </Card.Body>
+            </Card>
+        );
+    }
+}
