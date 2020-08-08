28aa03fd7b666785b4ee03728c11812699ed9dfd
saas-developer <ashwin.puthige@outlook.com>
2020-08-08 10:48:36 -0700

    Created PaymentMethod from Credit Card

--- client/src/payments/CheckoutForm.js
+++ client/src/payments/CheckoutForm.js
@@ -6,6 +6,48 @@ export default function CheckoutForm() {
   const stripe = useStripe();
   const elements = useElements();
 
+  const handleSubmit = async (event) => {
+      // We don't want to let default form submission happen here,
+      // which would refresh the page.
+      event.preventDefault();
+
+      if (!stripe || !elements) {
+        // Stripe.js has not yet loaded.
+        // Make sure to disable form submission until Stripe.js has loaded.
+        return;
+      }
+      // Get a reference to a mounted CardElement. Elements knows how
+      // to find your CardElement because there can only ever be one of
+      // each type of element.
+      const cardElement = elements.getElement(CardElement);
+
+      const { error, paymentMethod } = await stripe.createPaymentMethod({
+        type: 'card',
+        card: cardElement,
+      });
+
+      if (error) {
+        console.log('[createPaymentMethod error]', error);
+      } else {
+        console.log('[PaymentMethod]', paymentMethod);
+        const paymentMethodId = paymentMethod.id;
+        console.log('[paymentMethodId]', paymentMethodId);
+        // if (latestInvoicePaymentIntentStatus === 'requires_payment_method') {
+        //   // Update the payment method and retry invoice payment
+        //   const invoiceId = localStorage.getItem('latestInvoiceId');
+        //   retryInvoiceWithNewPaymentMethod({
+        //     customerId,
+        //     paymentMethodId,
+        //     invoiceId,
+        //     priceId,
+        //   });
+        // } else {
+        //   // Create the subscription
+        //   createSubscription({ customerId, paymentMethodId, priceId });
+        // }
+      }
+    };
+
   return (
     <form onSubmit={handleSubmit}>
       <CardSection />
